b9c4e44e6885b81eed7b9b0d5d7c0ea77be9e7b5
galibremo <galibremo@gmail.com>
2026-05-12 15:35:06 +0600

    feat: enhance fetchApi function to support custom headers and body serialization

--- src/lib/fetch-api.ts
+++ src/lib/fetch-api.ts
@@ -1,5 +1,18 @@
 const apiRoutePrefix = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
-export const fetchApi = (path: string, options?: RequestInit) => {
-  return fetch(`${apiRoutePrefix}${path}`, options);
+export const fetchApi = (
+  path: string,
+  options: RequestInit & { body?: any } = {},
+) => {
+  return fetch(`${apiRoutePrefix}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    body:
+      options.body && typeof options.body !== "string"
+        ? JSON.stringify(options.body)
+        : options.body,
+  });
 };
